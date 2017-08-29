dfbad00a9edafc058c6020aa5648b191f762fbf0
Kevin Herda <kherda@sgcmail.com>
2017-08-29 11:21:17 -0500

    updates

--- config/k8s/create.sh
+++ config/k8s/create.sh
@@ -1,4 +1,38 @@
 #!/usr/bin/env bash
 
+notes
+
+gcloud config set compute/zone us-central1-a
+
+demo-php
+
+gcloud container clusters create demo-php \
+  --num-nodes 3 \
+  --scopes "https://www.googleapis.com/auth/projecthosting,storage-rw"
+
+gcloud container clusters get-credentials {demo}
+
+
+gcloud compute images create jenkins-home-image --source-uri https://storage.googleapis.com/solutions-public-assets/jenkins-cd/jenkins-home-v3.tar.gz
+gcloud compute disks create jenkins-home --image jenkins-home-image
+
+kubectl create secret generic jenkins --from-file=options --namespace=jenkins
+
+kubectl get pods --namespace jenkins
+
+kubectl get svc --namespace jenkins
+
+kubectl get ingress --namespace jenkins
+
+kubectl describe ingress jenkins --namespace jenkins
+
+
+
+
+kubectl create namespace production
+create namespace
+list namespaces
+
+
 kubectl create -f config/k8s/replication_controller.yml
 kubectl create -f config/k8s/service.yml
--- config/k8s/scale-down.sh
+++ config/k8s/scale-down.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-kubectl -s http://127.0.0.1:8080 scale rc --replicas=1 k8s-php-test
+kubectl -s scale rc --replicas=1 k8s-php-test
